d5064a2e3063aca0b8076c858deb22ab3dea24c4
hyunjung <hhhj.1486@gmail.com>
2023-02-04 20:14:58 +0900

    init setting

---
+++ src/main/java/com/holaworld/holaworldrefactoring/HolaworldRefactoringApplication.java
@@ -0,0 +1,13 @@
+package com.holaworld.holaworldrefactoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolaworldRefactoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HolaworldRefactoringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/holaworld/holaworldrefactoring/HolaworldRefactoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holaworld.holaworldrefactoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolaworldRefactoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
